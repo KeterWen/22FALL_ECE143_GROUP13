a9b9cf7cf0a9652ed6c01206a6e4a9e095b650e0
KeterWen <liwen@ucsd.edu>
2022-11-22 11:37:51 -0800

    add plot utils and data utils

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -14,48 +14,7 @@ from sklearn.linear_model import LinearRegression
 from matplotlib.pyplot import MultipleLocator
 warnings.filterwarnings('ignore')
 
-
-def generate_dataset(df_maths, df_port, random_seed=0):
-    """
-    generate train/test dataset
-    :param df_maths: DataFrame of maths
-    :param df_port: DataFrame of portuguese
-    :param random_seed: random seed when split datasets
-    :return:
-    """
-
-    df_maths_label = df_maths.copy()
-    df_port_label = df_port.copy()
-
-    LabelEnc = preprocessing.LabelEncoder()
-
-    categorical_features = df_maths_label.select_dtypes(include=['object']).columns
-    for Feature in categorical_features:
-        df_maths_label[Feature] = LabelEnc.fit_transform(df_maths_label[Feature])
-
-    for Feature in categorical_features:
-        df_port_label[Feature] = LabelEnc.fit_transform(df_port_label[Feature])
-
-    ss = preprocessing.StandardScaler()
-
-    Xm = df_maths_label.copy()
-    Xm.drop(['G1', 'G2', 'G3'],axis = 1,inplace = True)
-    ym = df_maths_label['G3']
-    X_trainm, X_testm, y_trainm, y_testm = train_test_split(Xm, ym, test_size=0.2,random_state=random_seed)
-    X_trainm = ss.fit_transform(X_trainm)
-    X_testm = ss.transform(X_testm)
-
-    Xp = df_port_label.copy()
-    Xp.drop(['G1', 'G2', 'G3'],axis = 1,inplace = True)
-    yp = df_port_label['G3']
-
-    X_trainp, X_testp, y_trainp, y_testp = train_test_split(Xp, yp, test_size=0.2,random_state=random_seed)
-    X_trainp = ss.fit_transform(X_trainp)
-    X_testp = ss.transform(X_testp)
-
-    return X_trainm, X_testm, X_trainp, X_testp
-
-def plot_hist(df_maths, df_port, column_i,  save_path='./plot/test.png'):
+def plot_hist(df_maths, df_port, column_i,  save_path='./plot/test.png', show=False):
     """
     plot and save the hist given column_i
     :param df_maths: DataFrame of maths
@@ -69,9 +28,11 @@ def plot_hist(df_maths, df_port, column_i,  save_path='./plot/test.png'):
     # plt.legend(labels=['Maths', 'Port'], loc=2, bbox_to_anchor=(1, 1))
     plt.legend()
     plt.savefig(save_path)
+    if show:
+        plt.show()
     plt.close()
 
-def plot_heatmap(df, save_path='./plot/heatmap.png'):
+def plot_heatmap(df, save_path='./plot/heatmap.png', show=False):
     """
     plot and save the heatmap given useful columns
     :param df_maths: DataFrame to plot
@@ -85,6 +46,8 @@ def plot_heatmap(df, save_path='./plot/heatmap.png'):
     sns.heatmap(df_corr.corr(),annot=True,cmap='GnBu')
 
     plt.savefig(save_path)
+    if show:
+        plt.show()
     plt.close()
 
 def plot_bar_goout_vs_DWalc(df_maths, df_port, save_path='./plot/bar_goout_vs_DWalc.png'):
@@ -367,9 +330,6 @@ def plot_bar_Fedu_vs_G(df_maths, df_port, save_path='./plot/bar_Fedu_vs_G.png'):
     plt.savefig(save_path)
     plt.show()
     plt.close()
-
-
-
 
 if __name__ == '__main__':
     if not os.path.exists('./plot'):
